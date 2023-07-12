4f3558d7dc0d2f882af39aa6a8b577e762fae10e
Paulo Vitor <patoassado@gmail.com>
2023-07-12 16:03:14 -0300

    Generate first block

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
  * Importing the Block class
  */
 //
-const BlockClass = require('./block.js');
+const BlockClass = require("./block.js");
 
 /**
  * Creating a block object
@@ -10,14 +10,19 @@ const BlockClass = require('./block.js');
 const block = new BlockClass.Block("Test Block");
 
 // Generating the block hash
-block.generateHash().then((result) => {
-	console.log(`Block Hash: ${result.hash}`);
-	console.log(`Block: ${JSON.stringify(result)}`);
-}).catch((error) => {console.log(error)});
+block
+  .generateHash()
+  .then((result) => {
+    console.log(`Block Hash: ${result.hash}`);
+    console.log(`Block: ${JSON.stringify(result)}`);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
 /**
  * Step 3: Run the application in node.js
- * 
+ *
  */
 
 // From the terminal: cd into Project folder
